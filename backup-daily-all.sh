ee91c26020509350724fe7ac39c1f5e784fd7644
Renan Toesqui Magalhães <renantmagalhaes@gmail.com>
2020-02-18 15:34:27 +0100

    Update backup-daily-all.sh

--- backup-daily-all.sh
+++ backup-daily-all.sh
@@ -8,11 +8,11 @@ DAY=$(date +"%d")
 MONTH=$(date +"%m")
 YEAR=$(date +"%Y")
 
-mkdir -p $bkp_path/$ANO/$MES/$DIA
+mkdir -p $bkp_path/$YEAR/$MONTH/$DAY
 
 #Go to path and dump all databases
 cd $bkp_path/$YEAR/$MONTH/$DAY
-mongodump --out $bkp_path/$ANO/$MES/$DIA
+mongodump --out $bkp_path/$YEAR/$MONTH/$DAY
 
 #Compress all database in individual files
 #for i in * ; do tar zcvf "mongo-backup-daily-${i%/}-full.tar" "$i"; done
